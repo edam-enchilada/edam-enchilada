c62e4fbf3d38ca5862908e8a03ffa40064c09cee
gcipriano <gcipriano>
2005-07-15 22:32:11 +0000

    Added for aggregation: represents all options available for a collection when aggregating.

---
+++ collection/AggregationOptions.java
@@ -0,0 +1,62 @@
+package collection;
+
+import java.util.*;
+
+/**
+ * Struct-like class to hold onto Collection-specific
+ * aggregation options.
+ * @author gregc
+ *
+ */
+public class AggregationOptions {
+	public static enum CombiningMethod { SUM, AVERAGE };
+	
+	// These are only relevant to ATOFMS:
+	public double peakTolerance = .4;
+	public boolean produceParticleCountTS = false;
+	public CombiningMethod combMethod = CombiningMethod.SUM;
+	public int[] mzValues;
+	public String mzString = "";
+
+	public void setMZValues(String mzString) {
+		this.mzString = mzString;
+		
+		if (mzString.trim().equals(""))
+			return;
+		
+		ArrayList<Integer> tempValues = new ArrayList<Integer>();
+		
+		String[] ranges = mzString.split(",");
+		for (int i = 0; i < ranges.length; i++) {
+			String range = ranges[i].trim();
+			String[] splitRange = range.split(" to ");
+			int low = Integer.parseInt(splitRange[0]);
+			int high = splitRange.length == 2 ? Integer.parseInt(splitRange[1]) : low;
+			
+			// Swap if the user got them backwards...
+			if (low > high) {
+				int temp = low;
+				high = temp;
+				low = high;
+			}
+			
+			if (low < -600 || low > 600 || high < -600 || high > 600)
+				throw new NumberFormatException();
+			
+			while (low <= high)
+				tempValues.add(low++);
+		}
+		
+		mzValues = getMZValueArray(tempValues);
+	}
+	
+	public int[] getMZValueArray(ArrayList<Integer> values) {
+		int[] tempValues = new int[values.size()];
+		for (int i = 0; i < values.size(); i++)
+			tempValues[i] = values.get(i);
+		
+		Arrays.sort(tempValues);
+		
+		return tempValues;
+	}
+}
