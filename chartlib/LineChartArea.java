a960d0f2722c28613befbdddc979048ffff3653e
inspired2apathy <inspired2apathy>
2006-07-10 21:02:38 +0000

    added drawing points!

--- chartlib/LineChartArea.java
+++ chartlib/LineChartArea.java
@@ -9,6 +9,7 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.geom.Line2D;
+import java.util.Arrays;
 import java.util.Iterator;
 
 import javax.swing.JFrame;
@@ -43,8 +44,17 @@ public class LineChartArea extends MetricChartArea {
 	 */
 	@Override
 	public void drawData(Graphics2D g2d) {
-		
-		GraphAxis actualYAxis = yAxis;
+		drawDataLines(g2d,dataset);
+		drawDataPoints(g2d,dataset);
+	}
+	
+	/**
+	 * Draws the data in a continuous line by drawing only one
+	 * data point per horizontal pixel.
+	 * 
+	 * @param g2d
+	 */
+	protected void drawDataLines(Graphics2D g2d,Dataset dataset){
 		Rectangle dataArea = getDataAreaBounds();
 		// these booleans show whether we've drawn indicators that 
 		// more data exist in each direction
@@ -74,9 +84,9 @@ public class LineChartArea extends MetricChartArea {
 				drawMorePointsIndicator(1, g2d);
 			}
 			else {
-				int xCoord = (int) (xAxis.relativePosition(curPoint.x) * dataArea.width);
+				int xCoord = (int) (dataArea.x+xAxis.relativePosition(curPoint.x) * dataArea.width);
 				double yCoord = (dataArea.y + dataArea.height 
-						- (actualYAxis.relativePosition(curPoint.y) * dataArea.height));
+						- (yAxis.relativePosition(curPoint.y) * dataArea.height));
 				
 				if (yCoord > 0 && yCoord <= (dataArea.y + dataArea.height) && xCoord >= 0 && xCoord < dataArea.width) {
 					if (coords[xCoord] == 0 || yCoord < coords[xCoord])
@@ -89,24 +99,68 @@ public class LineChartArea extends MetricChartArea {
 		// Then draws them:
 		int lastX = 0;
 		double lastY = -999.0;
+		int numPoints = 0;
+		//boolean firstPoint = true;
 		for (int i = 0; i < coords.length; i++) {
 			if (coords[i] == 0)
 				continue;
 
-			int xPos = dataArea.x + i;
-			
-			if (coords[i] != -999.0 && lastY != -999.0)
+			int xPos = i;
+			if (coords[i] != -999.0 && lastY != -999.0){
+				System.out.println("got here 1: "+xPos+"\t"+coords[i]);
 				g2d.draw(new Line2D.Double((double) lastX, lastY, (double) xPos, coords[i]));
+				numPoints++;
+			}
 			else if (coords[i] != -999.0) {
 				// Point is valid, but last point wasn't... so just draw a large point:
-
-				g2d.setStroke(new BasicStroke(2.5f));
-				g2d.draw(new Line2D.Double((double) xPos, coords[i], (double) xPos, coords[i]));
-				g2d.setStroke(new BasicStroke(1.5f));
+				System.out.println("got here 2: "+xPos+"\t"+coords[i]);
+				g2d.draw(new Line2D.Double((double) dataArea.x+0,(dataArea.y + dataArea.height 
+						- (yAxis.relativePosition(0) * dataArea.height)), (double) xPos, coords[i]));
+				numPoints++;
 			}
 			
 			lastX = xPos;
 			lastY = coords[i];
+		}
+		
+		if(lastX<=dataArea.x+coords.length-1){
+			g2d.draw(new Line2D.Double((double) lastX, lastY, (double) dataArea.x+coords.length-1,(dataArea.y + dataArea.height 
+				- (yAxis.relativePosition(0) * dataArea.height))));
+		}
+		//cleanup
+		g2d.setClip(oldClip);
+		g2d.setStroke(oldStroke);
+		
+	}
+
+	private void drawDataPoints(Graphics2D g2d, Dataset dataset) {
+		GraphAxis actualYAxis = yAxis;
+		Rectangle dataArea = getDataAreaBounds();
+		
+		Shape oldClip = g2d.getClip();
+		Stroke oldStroke = g2d.getStroke();
+		g2d.setColor(Color.BLACK);
+		g2d.setStroke(new BasicStroke(4.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+		
+		int maxX = 0;
+		
+		//	loops through all data points, drawing each one as
+		//  a scatter plot...
+		Iterator<DataPoint> iterator = dataset.iterator();
+		while(iterator.hasNext())
+		{
+			DataPoint curPoint = iterator.next();
+			
+			double x = curPoint.y, y = curPoint.y;
+				
+			int xCoord = (int) (dataArea.x+xAxis.relativePosition(curPoint.x) * dataArea.width);
+			double yCoord = (dataArea.y + dataArea.height 
+					- (actualYAxis.relativePosition(curPoint.y) * dataArea.height));
+			System.out.println("got here: "+xCoord+"\t"+yCoord);
+			g2d.draw(new Line2D.Double((double)xCoord-1, yCoord-1, (double)xCoord+1, yCoord+1));
+			g2d.draw(new Line2D.Double((double)xCoord+1, yCoord-1, (double)xCoord+1, yCoord-1));
+			
+			//drawPoint(g2d,x,y);
 		}
 		
 		
@@ -114,7 +168,7 @@ public class LineChartArea extends MetricChartArea {
 		g2d.setClip(oldClip);
 		g2d.setStroke(oldStroke);
 	}
-
+	
 	/**
 	 * @param args
 	 */
