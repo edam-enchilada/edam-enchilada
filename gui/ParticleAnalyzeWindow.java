a26e91e9073c7a2639f8f5918158427bed32e614
rescdsk <rescdsk>
2006-07-21 14:32:52 +0000

    fixed spacing

--- gui/ParticleAnalyzeWindow.java
+++ gui/ParticleAnalyzeWindow.java
@@ -544,7 +544,7 @@ implements MouseMotionListener, MouseListener, ActionListener, KeyListener {
 		//chart.setDataDisplayType(true, false);
 		
 		chart.packData(false, true); //updates the Y axis scale.
-		chart.setTitle("Particle from" + filename);
+		chart.setTitle("Particle from " + filename);
 		double xMax = chart.getXRange()[1];
 		zchart.setCScrollMax(DEFAULT_XMAX > xMax ? DEFAULT_XMAX : xMax);
 		zchart.zoom(DEFAULT_XMIN, DEFAULT_XMAX);
