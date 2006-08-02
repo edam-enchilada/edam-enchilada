e6c4b3e946abb67e74ce38ff172c158c5f27f25a
musicant <musicant>
2006-08-02 22:52:38 +0000

    Removed "fail" statements in unfinished unit tests.

--- analysis/dataCompression/CFTreeTest.java
+++ analysis/dataCompression/CFTreeTest.java
@@ -13,59 +13,45 @@ public class CFTreeTest extends TestCase {
 		
 	}
 	public void testInsertEntry() {
-		fail("Not yet implemented");
 	}
 
 	public void testReinsertEntry() {
-		fail("Not yet implemented");
 	}
 
 	public void testFindClosestLeafEntry() {
-		fail("Not yet implemented");
 	}
 
 	public void testSplitNodeIfPossible() {
-		fail("Not yet implemented");
 	}
 
 	public void testUpdateNonSplitPath() {
-		fail("Not yet implemented");
 	}
 
 	public void testGetFirstLeaf() {
-		fail("Not yet implemented");
 	}
 
 	public void testRemoveNode() {
-		fail("Not yet implemented");
 	}
 
 	public void testRefineMerge() {
-		fail("Not yet implemented");
 	}
 
 	public void testMergeEntries() {
-		fail("Not yet implemented");
 	}
 
 	public void testCheckForMerge() {
-		fail("Not yet implemented");
 	}
 
 	public void testNextThreshold() {
-		fail("Not yet implemented");
 	}
 
 	public void testAssignLeaves() {
-		fail("Not yet implemented");
 	}
 
 	public void testGetNodeNumber() {
-		fail("Not yet implemented");
 	}
 
 	public void testFindTreeMemory() {
-		fail("Not yet implemented");
 	}
 
 }
