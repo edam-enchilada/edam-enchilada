d3c92a600f24e1c7bc48ee95dccafda97349b30f
mrcaps <mrcaps>
2006-08-03 16:49:01 +0000

    New unit test, previous ones work now

--- dataImporters/ATOFMSDataSetImporterTest.java
+++ dataImporters/ATOFMSDataSetImporterTest.java
@@ -47,11 +47,13 @@ import java.awt.Window;
 import java.io.File;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.zip.DataFormatException;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.event.TableModelEvent;
 
+import ATOFMS.CalInfo;
 import atom.ATOFMSAtomFromDB;
 import atom.GeneralAtomFromDB;
 
@@ -160,6 +162,15 @@ public class ATOFMSDataSetImporterTest extends TestCase {
 
 		importer.collectTableInfo();
 
+		synchronized (importer) {
+			try {
+				importer.wait();
+			}
+			catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
 		Statement stmt = db.getCon().createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM ATOFMSAtomInfoDense " +
 				"ORDER BY AtomID");
@@ -173,13 +184,15 @@ public class ATOFMSDataSetImporterTest extends TestCase {
 //					+",rs.get" + rsmd.getColumnClassName(i) +"("+i+"));");
 //			
 //		}
-//		
+		
+		String dir = System.getProperty("user.dir");
+
 		assertEquals(1,rs.getInt(1));
 		assertEquals("2004-08-04 15:39:00.0",rs.getString(2));
 		assertEquals(1.031E-6,rs.getFloat(3), 0.0001);
 		assertEquals(0.0,rs.getFloat(4), 0.0001);
 		assertEquals(3129,rs.getInt(5));
-		assertEquals("C:\\Documents and Settings\\smitht\\my documents\\workspace\\edam-enchilada\\testRow\\b\\b-040804153913-00001.amz",rs.getString(6));
+		assertEquals(dir + "\\testRow\\b\\b-040804153913-00001.amz",rs.getString(6));
 		
 		while (rs.next()) {
 			rowCount++;
@@ -192,7 +205,121 @@ public class ATOFMSDataSetImporterTest extends TestCase {
 			assertEquals(i, atomIDs.get(i).intValue());
 		
 		assertEquals("b", db.getCollectionName(2));
+	}
+	
+	/**
+	 * Creates some functionality for a file that can be modified and then have its contents reset.
+	 * Potentially useful for writing tests that depend upon files to run.
+	 * Must invoke close() to write back the original contents.
+	 * @author shaferia
+	 */
+	public static class ResettableFile {
+		private File file;
+		private String current;
+		private String original;
 		
+		public ResettableFile(File f) {
+			this(f.getAbsolutePath());
+		}
 		
+		public ResettableFile(String filename) {
+			file = new File(filename);
+			
+			if (!file.exists())
+				throw new IllegalArgumentException("ResettableFile: file doesn't exist: " + filename);
+			
+			original = readFile(file);
+		}
+		
+		private String readFile(File f) {
+			try {
+				StringBuffer in = new StringBuffer();
+				java.io.FileReader fw = new java.io.FileReader(f);
+				while (fw.ready()) {
+					in.append((char) fw.read());
+				}
+				return in.toString();
+			}
+			catch (java.io.IOException ex) {
+				System.err.println("ResettableFile: couldn't read file: " + f.getAbsolutePath());
+				return null;
+			}
+		}
+		
+		private void writeFile(File f, String contents) {
+			try {
+				java.io.FileWriter fw = new java.io.FileWriter(f);
+				fw.write(contents);
+			}
+			catch (java.io.IOException ex) {
+				System.err.println("ResettableFile: couldn't write file: " + f.getAbsolutePath());
+			}			
+		}
+		
+		public String getCurrent() {
+			return current;
+		}
+		
+		public void setCurrent(String current) {
+			this.current = current;
+			writeFile(file, this.current);
+		}
+		
+		public String getOriginal() {
+			return original;
+		}
+		
+		public void replaceFirst(String regex, String replacement) {
+			current.replaceFirst(regex, replacement);
+			writeFile(file, current);
+		}
+		
+		public void replaceAll(String regex, String replacement) {
+			current.replaceAll(regex, replacement);
+			writeFile(file, current);
+		}
+		
+		public void close() {
+			writeFile(file, original);
+		}
+	}
+	
+	public void testReadParFileAndCreateEmptyCollection() {
+		try {
+			importer.parFile = new File((String)table.getValueAt(0,1));
+			ATOFMS.ATOFMSParticle.currCalInfo = new CalInfo("testRow\\b\\cal.cal", true);
+			ATOFMS.ATOFMSParticle.currPeakParams = new ATOFMS.PeakParams(10, 20, .1f, .5f);			
+		}
+		catch (java.io.IOException ex) {
+			fail("Couldn't open necessary files to create empty collection");
+		}
+		
+		try {
+			importer.readParFileAndCreateEmptyCollection();
+		}
+		catch (java.io.IOException ex) {
+			fail();
+		}
+		catch (DataFormatException ex) {
+			fail();
+		}
+		
+		try {
+			Connection con = db.getCon();
+			
+			ResultSet rs = con.createStatement().executeQuery(
+					"SELECT * FROM Collections WHERE CollectionID = 2");
+
+			assertTrue(rs.next());
+			assertEquals(rs.getString("Comment"), "ambient");
+			assertEquals(rs.getString("Description"), "b: ambient");
+			assertEquals(rs.getString("Datatype"), "ATOFMS");
+			
+			assertFalse(rs.next());
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+			fail("Couldn't analyze success of readParFileAndCreateEmptyCollection");
+		}
 	}
 }
