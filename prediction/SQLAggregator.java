4e35da4e0ce7b87b2a0ce124a16ace76b9cf18cf
musicant <musicant>
2006-12-23 14:56:32 +0000

    Lots and lots of changes

--- prediction/SQLAggregator.java
+++ prediction/SQLAggregator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -28,6 +29,9 @@ import database.InfoWarehouse;
  */
 
 public class SQLAggregator {
+	
+	// Set maxAtomId to 0 to run the whole thing
+	public static final int maxAtomId = 0;
 
 	private InfoWarehouse db;
 	private Connection con;
@@ -43,75 +47,139 @@ public class SQLAggregator {
 	
 	/**
 	 * @author steinbel
-	 * Takes a string with the time and bc or ec level, gathers and formats the
-	 * ATOFMS data appropriately.
-	 * @param in - the string with the time and ec or bc level
-	 * @return a formatted string for Weka with ATOFMS info added
+	 * @author dmusican
+	 * Gathers and formats the ATOFMS data appropriately.
+	 * @param outputFile - name of the output file
 	 */
-	public String process(String in){
-		//start the output string
-		String output = "{" + "0 " + '"' + in.split(",")[0] + '"' +
-						",1 " + in.split(",")[1] + ",";
-		
+	public void process(PrintWriter out) throws IOException{
 		//send incoming string to create a temp table
-		createTempTable(in);
+		createTempTable();
+
 		System.out.println("table created");//TESTING
-		BinnedPeakList bpl = new BinnedPeakList();
-		double mass = 0.0;
+
 		try {
-			Statement stmt = con.createStatement();
+			// Calculate total mass for each time bin, and connect it with
+			// EC data. Throw away any bins with too few particles
+			String denseQuery =
+				"SELECT roundedTime, mass, value FROM\n" +
+				"(SELECT roundedTime, COUNT(*) as cnt,\n" +
+				"	SUM(size*size*size*" + density + "*(1/de)) as mass\n" +
+				"   FROM RoundedDense\n";
+			if (maxAtomId > 0)
+				denseQuery += " WHERE atomID <= " + maxAtomId + "\n"; 
+			denseQuery +=
+				"   GROUP BY roundedTime\n" +
+				"   HAVING COUNT(*) > 200) Masses, AggData\n" +
+				"WHERE Masses.roundedTime = AggData.Timestamp\n" +
+				"AND value <> 0\n" +
+				"ORDER BY roundedTime";
+
+			System.out.println(denseQuery);
+			Statement denseStmt = con.createStatement();
+			ResultSet denseSet = denseStmt.executeQuery(denseQuery); 
+
 			/* Sum together the sizes of the particles and the areas of the
 			 * peaks, both adjusted for detection efficiency. 
 			 */
-			String query = "select sum(d.size*d.size*d.size*" + density+ "*(1/h.de)) as mass," +
-					" s.peaklocation, sum(s.peakarea/h.de) as adjustedpeak " +
-					"from atofmsatominfodense d, atofmsatominfosparse s, #hourbin h " +
-					"where h.atom = s.atomid and h.atom = d.atomid " +
-					"group by s.peaklocation;";
-			System.out.println(query);
-			ResultSet rs = stmt.executeQuery(query);
-			while (rs.next()){
-				mass += rs.getDouble("mass");
-				bpl.add(rs.getFloat("peaklocation"), rs.getFloat("adjustedpeak"));
+			String sparseQuery =
+				"SELECT roundedTime, peaklocation,\n" +
+				"	SUM(peakarea/de) as adjustedpeak\n" +
+				"FROM RoundedDense d, ATOFMSAtomInfosparse s\n" +
+				"WHERE d.atomid = s.atomid\n";
+			if (maxAtomId > 0)
+				sparseQuery += "AND d.atomID <= " + maxAtomId + "\n"; 
+			sparseQuery +=
+				"GROUP BY roundedTime, peaklocation\n" +
+				"ORDER BY roundedTime, peaklocation";
+			System.out.println(sparseQuery);
+			Statement sparseStmt = con.createStatement();
+			ResultSet sparseSet = sparseStmt.executeQuery(sparseQuery);
+
+			boolean denseRead = denseSet.next();
+			boolean sparseRead = sparseSet.next();
+			boolean newMass = true;
+
+			if (!denseRead)
+				throw new RuntimeException("Dense set empty");
+			if (!sparseRead)
+				throw new RuntimeException("Sparse set empty");
+			
+			while (denseRead) {
+				Timestamp denseTime = denseSet.getTimestamp("roundedTime");
+				Timestamp sparseTime = sparseSet.getTimestamp("roundedTime");
+
+				// If denseTime is greater than sparseTime, we may have thrown
+				// away that dense row because mass was 0. Skim through sparse
+				// time until we match
+				while (denseTime.compareTo(sparseTime) > 0) {
+					sparseRead = sparseSet.next();
+					if (sparseRead) {
+						sparseTime = sparseSet.getTimestamp("roundedTime");
+					} else {
+						throw new RuntimeException("No more sparse data");
+					}
+				}
+				
+				if (!denseTime.equals(sparseTime)) {
+					// Should never happen, this is error checking
+					throw new RuntimeException("Dense time does not equal sparse time");
+				}
+				
+				// Only write out the row of the value is nonzero. If the mass
+				// is zero, this corresponds to missing data.
+				
+				float value = denseSet.getFloat("value");
+				
+				// Starting new dense, write out the header. Chop off the ".0"
+				// at the end of the time that seems to give Weka trouble.
+				String wekaTime = (denseTime.toString().split("\\."))[0];
+
+				// Adjust the units of mass to put it on a smaller scale:
+				// this makes tools such as Weka happier.
+				float massScaleFactor = 1e8f;
+				out.print("{" + "0 " + '"' + wekaTime + '"' +
+						",1 " + denseSet.getFloat("value") +
+						",2 " + denseSet.getFloat("mass")/massScaleFactor);
+
+				// Loop over all sparse data with matching time
+				while (sparseRead && denseTime.equals(sparseTime)) {
+
+					// Write out the sparse data. Adjust the units
+					// of adjustpeak to put it on a smaller scale:
+					// this makes tools such as Weka happier.
+					float peakScaleFactor = 1e8f;
+					int location = sparseSet.getInt("peaklocation");
+					if ((location >= -300) && (location <= 300)){
+						out.print("," + (location+303) + " " +
+								sparseSet.getFloat("adjustedpeak") /
+								peakScaleFactor);
+					}
+
+					// Grab next sparse row
+					sparseRead = sparseSet.next();
+					if (sparseRead) {
+						sparseTime = sparseSet.getTimestamp("roundedTime");
+					}
+
+				}
+
+				// Write out end of row
+				out.println("}");
+
+				denseRead = denseSet.next();
 			}
-			rs.close();
-			stmt.close();
+			denseSet.close();
+			sparseSet.close();
+			denseStmt.close();
+			sparseStmt.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		bpl.printPeakList();//TESTING
-		//format the peaklist for the .arff format
-		 output += format(bpl, mass);
-		 output += "}\n";
-		
-		//drop temp table
-		dropTempTable();
-		
-		return output;
-	}
-	
-	/**
-	 * @author steinbel
-	 * Formats the mass and the peaks for feeding into Weka.
-	 * @param list - the list of peaks and locations for this time bin
-	 * @param m - the mass found during this time bin
-	 * @return a string formatted for Weka
-	 */
-	private String format(BinnedPeakList list, double m){
-		String out = "";
-		out += "2 " + m;
-		
-		for (BinnedPeak peak : list) {
-			int location = peak.key;
-			//this is where the limit on peak locations recorded takes effect
-			if ((location >= -300) && (location <= 300)){
-				out += "," + (location+303) + " " + peak.value;
-			}
-		}
 
-		return out;
+		//drop temp table
+		//dropTempTable();
+		
 	}
 	
 	/**
@@ -133,56 +201,63 @@ public class SQLAggregator {
 	
 	/**
 	 * @author steinbel
-	 * Creates a temporary table of atomID and detection efficiency of those 
-	 * particles that were observed during the time period centered on the
-	 * time given in the line of text coming into this method.
-	 * @param lineIn - the time of the bc/ec collection and the amount of ec/bc
+	 * @author dmusican
+	 * Creates a temporary table with all atomIDs of interest, and calculates
+	 * necessary detection efficiency.
 	 */
-	private void createTempTable(String lineIn){
-		//grab the time & date off the line coming in, create temp table
-		String orig = lineIn.split(",")[0];
-		SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy hh:mm:ss a");
+	private void createTempTable(){
 		try {
-			Date given = sdf.parse(orig);
-			Timestamp ts = new Timestamp(given.getTime());
-			//this may be crazy and convoluted, but SQLServer likes the results.
-			String time = ts.toString().substring(0, ts.toString().lastIndexOf('.'));
 			Statement stmt = con.createStatement();
 			/* Create a temp table with atomID and detection efficiency 
 			 * (initially populated with the size of the particles) during the
 			 * hour-long timebin around the given time.
 			 */
-			stmt.executeUpdate("IF (OBJECT_ID('tempdb..#hourbin') IS NOT NULL) DROP TABLE #hourbin;");
-			String order = "create table #hourbin (atom int, de real);" +
-			" insert into #hourbin(atom, de)" +
-			" select d.atomid, d.size" +
-			" from ATOFMSAtomInfoDense d" +
-			" where d.time < (DATEADD (n, 30, '"+ time + "'))" +
-			" and d.time >= (dateadd (n, -30, '"+ time + "'));";
+			dropTempTable();
+
+			System.out.println("Creating temp table.");
+			String order = 
+				"CREATE TABLE RoundedDense (\n" +
+				"	atomid int,\n" +
+				"	roundedTime DATETIME,\n" +
+				"	size REAL,\n" +
+				"	de REAL)\n" +
+				"INSERT INTO RoundedDense (atomid, roundedTime, size, de)\n" +
+				"SELECT atomid,\n" +
+				//"	DATEADD(hour, DATEDIFF(hour, '20000101', DATEADD(minute, 30, time)), '20000101') as roundedTime,\n" +
+				"	DATEADD(hour, DATEDIFF(hour, '20000101', time), '20000101') as roundedTime,\n" +
+				//"	DATEADD(hour, DATEDIFF(hour, '20000101', DATEADD(minute, 60, time)), '20000101') as roundedTime,\n" +
+				"	size, NULL\n" +
+				"FROM ATOFMSAtomInfoDense\n" +
+				"WHERE size >= 0.1 AND size <= 2.5\n";
+			if (maxAtomId > 0)
+				order += "AND atomID <= " + maxAtomId; 
+			
 			System.out.println(order);//TESTING
 			stmt.executeUpdate(order);
-			//remove particles outside of size range
-			stmt.executeUpdate("delete from #hourbin" +
-					" where de < .1 or de > 2.5;");
+
 			//calculate the Detection Efficiency for the three size bins
 			//as described in DetectionEfficiency_Gomit.pdf
-			// Note that the below code is technically not always right: if
-			// updating the de from one query puts in the range of the next one,
-			// will get updated multiple times. Not fixing for now because
-			// will rewrite this anyway.
-			stmt.executeUpdate("update #hourbin " +
-					"set de = (select power(de*1000, 2.8574)*exp(-27.16)) " +
-					"where de > .1 and de <= .75;");
-			stmt.executeUpdate("update #hourbin " +
-					"set de = (select power(de*1000, -.58272)*exp(-4.803)) " +
-					"where de > .75	and de <= 1.;");
-			stmt.executeUpdate("update #hourbin " +
-					"set de = (select power(de*1000, -7.52)*exp(42.031)) " +
-					"where de > 1. and de <= 2.5;");
+			System.out.println("Update 1");
+			stmt.executeUpdate("update RoundedDense " +
+					"set de = (select power(size*1000, 2.8574)*exp(-27.16)) " +
+					"where size >= .1 and size <= .75;");
+			System.out.println("Update 2");
+			stmt.executeUpdate("update RoundedDense " +
+					"set de = (select power(size*1000, -.58272)*exp(-4.803)) " +
+					"where size > .75 and size < 1.;");
+			System.out.println("Update 3");
+			stmt.executeUpdate("update RoundedDense " +
+					"set de = (select power(size*1000, -7.52)*exp(42.031)) " +
+					"where size >= 1. and size <= 2.5;");
+			
+			// Verify that all de rows have been calculated
+			ResultSet rs = 
+				stmt.executeQuery("SELECT * FROM RoundedDense WHERE de IS NULL");
+			if (rs.next())
+				throw new RuntimeException("de not calculated for all values");
+			rs.close();
+			
 			stmt.close();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -197,9 +272,9 @@ public class SQLAggregator {
 	private void dropTempTable(){
 		try {
 			Statement stmt = con.createStatement();
-			stmt.executeUpdate("IF (OBJECT_ID('tempdb..#hourbin') " +
+			stmt.executeUpdate("IF (OBJECT_ID('RoundedDense') " +
 					"IS NOT NULL)\n" +
-					" DROP TABLE #hourbin\n");
+					" DROP TABLE RoundedDense\n");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -209,6 +284,7 @@ public class SQLAggregator {
 
 	/**
 	 * @author steinbel
+	 * @author dmusican
 	 * Writes the .arff header info for this file, which includes naming
 	 * the m/z values as attributes so we can find them later. 
 	 * @param relationName - the name of the relation
@@ -217,7 +293,7 @@ public class SQLAggregator {
 	 */
 	public String assembleAttributes(String relationName, String predictThis){
 		String attributeNames = "@relation " + relationName +"\n"
-				+"@attribute time date \"MM/d/yyyy hh:mm:ss a\" \n"
+				+"@attribute time date \"yyyy-MM-dd HH:mm:ss\" \n"
 				+"@attribute " + predictThis + " numeric \n"
 				+"@attribute mass numeric \n";
 		
@@ -230,29 +306,54 @@ public class SQLAggregator {
 		return attributeNames;
 	}
 	
+	/**
+	 * @author dmusican
+	 * Import the filter data
+	 */
+	private void importFilterData(String filename) {
+		try {
+			Statement stmt = con.createStatement();
+
+			stmt.executeUpdate(
+				"IF (OBJECT_ID('AggData') IS NOT NULL) DROP TABLE AggData"
+			);
+			
+			stmt.executeUpdate(
+				"CREATE TABLE AggData (TimeStamp DATETIME, Value FLOAT)"
+			);
+
+			stmt.executeUpdate(
+				"BULK INSERT AggData\n" +
+				"FROM '" + filename + "'\n" + 
+				"WITH (FIELDTERMINATOR = ',', ROWTERMINATOR = '\n')"
+			);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 	
 //	take in file of ec/bc/whatever data in csv format
 	public static void main(String[] args){
 		Date start = new Date();
-		String line;
-		String result = "";
 		StringBuilder builder = new StringBuilder();
 		SQLAggregator sa = new SQLAggregator();
+		sa.open();
 		try {
-			Scanner scan = new Scanner(new File("C:/Documents and Settings/dmusican/workspace/edam-enchilada/prediction/verysmall.csv"));
-			FileWriter out = new FileWriter(new File("C:/Documents and Settings/dmusican/workspace/edam-enchilada/prediction/verysmall.arff")); 
+			PrintWriter out = new PrintWriter("C:/Documents and Settings/dmusican/workspace/edam-enchilada/prediction/EC200.arff"); 
 			//write the .arff file headings
-			out.write(sa.assembleAttributes("small", "ec"));
-			sa.open();
-			//aggregate data and format into .arff format, write to file
-			while (scan.hasNextLine()){
+			out.print(sa.assembleAttributes("ecrelation", "ec"));
 
-				line = scan.nextLine();
-				out.append(sa.process(line));
-			}
-			sa.close();
+			//aggregate data and format into .arff format, write to file
+			sa.importFilterData("C:/Documents and Settings/dmusican/workspace/edam-enchilada/prediction/EC.csv");
+			System.out.println("Data imported");
+			
+			sa.process(out);
 			out.close();
-			scan.close();
+			sa.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
