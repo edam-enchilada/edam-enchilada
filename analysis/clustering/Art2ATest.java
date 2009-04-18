e891d02d5e81fcfffb08cf18944f5a2819c203c7
jtbigwoo <jtbigwoo>
2009-04-18 16:50:17 +0000

    just updated the output comments after fixing defect 2650025

--- analysis/clustering/Art2ATest.java
+++ analysis/clustering/Art2ATest.java
@@ -119,84 +119,63 @@ public class Art2ATest extends TestCase{
 	    	assertTrue(cluster3.getSubCollectionIDs().isEmpty());
 	    	
 	    	/** Output:
-Pass #:0
-Adding new centroid
-Adding new centroid
-Adding new centroid
-about to reset
-Pass #:1
-about to reset
-Pass #:2
-about to reset
-Pass #:3
-about to reset
-Pass #:4
-about to reset
-Pass #:5
-about to reset
-Pass #:6
-about to reset
-Pass #:7
-about to reset
-Pass #:8
-about to reset
-Pass #:9
-about to reset
-Pass #:10
-about to reset
-Pass #:11
-about to reset
-Clustering Parameters: 
-Art2A,V=1.0,LR=0.0050,Passes=25,DMetric=CITY_BLOCK
-
-
-Number of ignored particles with zero peaks = 1
-Total clustering passes during sampling = 0
-Total number of centroid clustering passes = 0
-Total number of passes = 13
-Average distance of all points from their centers at each iteration:
-0.166666641831398
-0.16667083650827408
-0.16667494922876358
-0.1666790321469307
-0.1666831150650978
-0.16668709367513657
-0.16669104248285294
-0.16669496148824692
-0.1666988506913185
-0.16670271009206772
-0.16670649498701096
-0.1667102947831154
-0.1666666641831398
-average distance of all points from their centers on final assignment:
-0.1666666641831398
-
-Peaks in centroids:
-Centroid 1: Number of particles = 2
-Centroid 2: Number of particles = 1
-Centroid 3: Number of particles = 1
-
-Centroid 1:
-Number of particles in cluster: 2
-Key:	Value:
--30	0.4905308
-30	0.4905308
-45	0.018938426
-Centroid 2:
-Number of particles in cluster: 1
-Key:	Value:
--30	0.25
--20	0.25
--10	0.25
-20	0.25
-Centroid 3:
-Number of particles in cluster: 1
-Key:	Value:
--300	0.2
--30	0.2
--20	0.2
-6	0.2
-30	0.2
+				Clustering Parameters: 
+				Art2A,V=1.0,LR=0.0050,Passes=25,DMetric=CITY_BLOCK,Test comment
+				
+				
+				Number of ignored particles with zero peaks = 1
+				Total clustering passes during sampling = 0
+				Total number of centroid clustering passes = 0
+				Total number of passes = 13
+				Average distance of all points from their centers at each iteration:
+				0.125
+				0.125003129534889
+				0.125006218906492
+				0.12500928447116166
+				0.12501231208443642
+				0.12501531769521534
+				0.12501828651875257
+				0.12502123322337866
+				0.12502414314076304
+				0.12502703070640564
+				0.12502990197390318
+				0.1250327043235302
+				0.12500003073364496
+				average distance of all points from their centers on final assignment:
+				0.12500003073364496
+				
+				Peaks in centroids:
+				Centroid 1: Number of particles = 2
+				Centroid 2: Number of particles = 1
+				Centroid 3: Number of particles = 1
+				
+				Centroid 1:
+				Number of particles in cluster: 2
+				Mean size: 0.25 Std dev: +/-0.05000005
+				Geometric mean size: 0.24494898
+				Key:	Value:
+				-30	5000.0005
+				30	4857.9614
+				45	142.03822
+				Centroid 2:
+				Number of particles in cluster: 1
+				Mean size: 0.4 Std dev: +/-0.0
+				Geometric mean size: 0.4
+				Key:	Value:
+				-30	1666.6667
+				-20	1666.6667
+				-10	1666.6667
+				20	5000.0
+				Centroid 3:
+				Number of particles in cluster: 1
+				Mean size: 0.5 Std dev: +/-0.0
+				Geometric mean size: 0.5
+				Key:	Value:
+				-300	1666.6667
+				-30	1666.6667
+				-20	1666.6667
+				6	2500.0
+				30	2500.0
 	    	 */
 	    	
 	    }
