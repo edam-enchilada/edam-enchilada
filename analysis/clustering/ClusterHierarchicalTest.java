7d3990080ef2309649dbec645930f803e77bd9a0
jtbigwoo <jtbigwoo>
2009-05-25 19:39:11 +0000

    updated to test the basic hierarchical clustering

--- analysis/clustering/ClusterHierarchicalTest.java
+++ analysis/clustering/ClusterHierarchicalTest.java
@@ -40,14 +40,22 @@
 
 package analysis.clustering;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Map;
 
+import collection.Collection;
+
+import ATOFMS.ParticleInfo;
 import analysis.CollectionDivider;
 
 import database.CreateTestDatabase;
 import database.InfoWarehouse;
 import database.Database;
 import junit.framework.TestCase;
+import database.CollectionCursor;
+
 /*
  * Created on April 23, 2009
  *
@@ -93,7 +101,7 @@ public class ClusterHierarchicalTest extends TestCase {
 		db.closeConnection();
 		System.runFinalization();
 		System.gc();
-	    Database.dropDatabase(dbName);
+//	    Database.dropDatabase(dbName);
     }
 
     public void testHierarchicalClustering() throws Exception {
@@ -102,72 +110,72 @@ public class ClusterHierarchicalTest extends TestCase {
     	
     	assertTrue(collectionID == 7);
     	
-//    	Collection clusterParent = db.getCollection(7);
-//    	BufferedReader descReader = new BufferedReader(new StringReader(clusterParent.getDescription()));
-//    	for (int i = 0; i < 4; i++) descReader.readLine();
-//    	assertEquals("Number of ignored particles with zero peaks = 0", descReader.readLine());
-//    	// the preceeding test assertion is actually incorrect, the correct test follows, see bug 2772661
-//    	//assertEquals("Number of ignored particles with zero peaks = 1", descReader.readLine());
-//
-//    	// check the std deviations in the collection description
-//    	for (int i = 0; i < 16; i++) descReader.readLine();
-//		assertEquals("Mean size: 0.33333334 Std dev: +/-0.12472187", descReader.readLine());
-//		assertEquals("Geometric mean size: 0.31072325", descReader.readLine());
-//		for (int i = 0; i < 9; i++) descReader.readLine();
-//		assertEquals("Mean size: 0.4 Std dev: +/-0.0", descReader.readLine());
-//		assertEquals("Geometric mean size: 0.4", descReader.readLine());
-//    	
-//    	Collection cluster1 = db.getCollection(8);
-//    	Collection cluster2 = db.getCollection(9);
-//    	Collection clusterCenters = db.getCollection(10);
-//
-//    	assertTrue(cluster1.containsData());
-//    	assertEquals("1", cluster1.getComment());
-//    	assertEquals("ATOFMS", cluster1.getDatatype());
-//    	assertTrue(cluster1.getDescription().startsWith("Key:\tValue:"));
-//    	assertEquals("1", cluster1.getName());
-//    	assertEquals(7, cluster1.getParentCollection().getCollectionID());
-//    	ArrayList<Integer> particles = cluster1.getParticleIDs();
-//       	assertEquals(2, particles.get(0).intValue());
-//    	assertEquals(3, particles.get(1).intValue());
-//    	assertEquals(5, particles.get(2).intValue());
-//    	assertTrue(cluster1.getSubCollectionIDs().isEmpty());
-//    	
-//    	assertTrue(cluster2.containsData());
-//    	assertEquals("2", cluster2.getComment());
-//    	assertEquals("ATOFMS", cluster2.getDatatype());
-//    	assertTrue(cluster2.getDescription().startsWith("Key:\tValue:"));
-//    	assertEquals("2", cluster2.getName());
-//    	assertEquals(7, cluster2.getParentCollection().getCollectionID());
-//    	particles = cluster2.getParticleIDs();
-//    	assertEquals(4, particles.get(0).intValue());
-//    	assertTrue(cluster2.getSubCollectionIDs().isEmpty());
-//
-//    	CollectionCursor denseCurs = db.getAtomInfoOnlyCursor(clusterCenters);
-//    	denseCurs.next();
-//    	ParticleInfo info = denseCurs.getCurrent();
-//    	assertEquals(0.33333334f, info.getATOFMSParticleInfo().getSize());
-//    	denseCurs.next();
-//    	info = denseCurs.getCurrent();
-//    	assertEquals(.4f, info.getATOFMSParticleInfo().getSize());
-//
-//    	CollectionCursor sparseCurs = db.getBPLOnlyCursor(clusterCenters);
-//    	sparseCurs.next();
-//    	info = sparseCurs.getCurrent();
-//    	Map<Integer, Float> peaks = info.getBinnedList().getPeaks();
-//    	assertEquals(555.0f, peaks.get(-300).floatValue());
-//    	assertEquals(3888.0f, peaks.get(-30).floatValue());
-//    	assertEquals(555.0f, peaks.get(-20).floatValue());
-//    	assertEquals(833.0f, peaks.get(6).floatValue());
-//    	assertEquals(3333.0f, peaks.get(30).floatValue());
-//    	assertEquals(833.0f, peaks.get(45).floatValue());
-//    	sparseCurs.next();
-//    	info = sparseCurs.getCurrent();
-//    	peaks = info.getBinnedList().getPeaks();
-//    	assertEquals(1666.0f, peaks.get(-30).floatValue());
-//    	assertEquals(1666.0f, peaks.get(-20).floatValue());
-//    	assertEquals(1666.0f, peaks.get(-10).floatValue());
-//    	assertEquals(5000.0f, peaks.get(20).floatValue());
+    	Collection clusterParent = db.getCollection(7);
+    	BufferedReader descReader = new BufferedReader(new StringReader(clusterParent.getDescription()));
+    	for (int i = 0; i < 4; i++) descReader.readLine();
+    	assertEquals("Number of ignored particles with zero peaks = 0", descReader.readLine());
+    	// the preceeding test assertion is actually incorrect, the correct test follows, see bug 2772661
+    	//assertEquals("Number of ignored particles with zero peaks = 1", descReader.readLine());
+
+    	// check the std deviations in the collection description
+    	for (int i = 0; i < 15; i++) descReader.readLine();
+		assertEquals("Mean size: 0.25 Std dev: +/-0.05000005", descReader.readLine());
+		assertEquals("Geometric mean size: 0.24494898", descReader.readLine());
+		for (int i = 0; i < 6; i++) descReader.readLine();
+		assertEquals("Mean size: 0.45 Std dev: +/-0.050000273", descReader.readLine());
+		assertEquals("Geometric mean size: 0.4472136", descReader.readLine());
+    	
+    	Collection cluster1 = db.getCollection(8);
+    	Collection cluster2 = db.getCollection(9);
+    	Collection clusterCenters = db.getCollection(10);
+
+    	assertTrue(cluster1.containsData());
+    	assertEquals("1", cluster1.getComment());
+    	assertEquals("ATOFMS", cluster1.getDatatype());
+    	assertTrue(cluster1.getDescription().startsWith("Key:\tValue:"));
+    	assertEquals("1", cluster1.getName());
+    	assertEquals(7, cluster1.getParentCollection().getCollectionID());
+    	ArrayList<Integer> particles = cluster1.getParticleIDs();
+       	assertEquals(2, particles.get(0).intValue());
+    	assertEquals(3, particles.get(1).intValue());
+    	assertTrue(cluster1.getSubCollectionIDs().isEmpty());
+    	
+    	assertTrue(cluster2.containsData());
+    	assertEquals("2", cluster2.getComment());
+    	assertEquals("ATOFMS", cluster2.getDatatype());
+    	assertTrue(cluster2.getDescription().startsWith("Key:\tValue:"));
+    	assertEquals("2", cluster2.getName());
+    	assertEquals(7, cluster2.getParentCollection().getCollectionID());
+    	particles = cluster2.getParticleIDs();
+    	assertEquals(4, particles.get(0).intValue());
+    	assertEquals(5, particles.get(1).intValue());
+    	assertTrue(cluster2.getSubCollectionIDs().isEmpty());
+
+    	CollectionCursor denseCurs = db.getAtomInfoOnlyCursor(clusterCenters);
+    	denseCurs.next();
+    	ParticleInfo info = denseCurs.getCurrent();
+    	assertEquals(0.25f, info.getATOFMSParticleInfo().getSize());
+    	denseCurs.next();
+    	info = denseCurs.getCurrent();
+    	assertEquals(.45f, info.getATOFMSParticleInfo().getSize());
+
+    	CollectionCursor sparseCurs = db.getBPLOnlyCursor(clusterCenters);
+    	sparseCurs.next();
+    	info = sparseCurs.getCurrent();
+    	Map<Integer, Float> peaks = info.getBinnedList().getPeaks();
+    	assertEquals(5000.0f, peaks.get(-30).floatValue());
+    	assertEquals(3750.0f, peaks.get(30).floatValue());
+    	assertEquals(1250.0f, peaks.get(45).floatValue());
+    	sparseCurs.next();
+    	info = sparseCurs.getCurrent();
+    	peaks = info.getBinnedList().getPeaks();
+    	assertEquals(833.0f, peaks.get(-300).floatValue());
+    	assertEquals(1666.0f, peaks.get(-30).floatValue());
+    	assertEquals(1666.0f, peaks.get(-20).floatValue());
+    	assertEquals(833.0f, peaks.get(-10).floatValue());
+    	assertEquals(1250.0f, peaks.get(6).floatValue());
+    	assertEquals(2500.0f, peaks.get(20).floatValue());
+    	assertEquals(1250.0f, peaks.get(30).floatValue());
     	
     	/** Output:
 			Clustering Parameters: 
